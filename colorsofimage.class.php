6c09c301e9470714b74f4547c358a1c25df193cc
Joe Hoyle <joehoyle@gmail.com>
2012-07-20 21:44:42 +0100

    fix islet check

--- colorsofimage.class.php
+++ colorsofimage.class.php
@@ -240,7 +240,7 @@ class ColorsOfImage {
 
 	private function getPalette() {
 
-		if ( isset( $this->_palette ) )
+		if ( ! empty( $this->_palette ) )
 			return $this->_palette;
 
 		$str = '["#660000", "#990000", "#cc0000", "#cc3333", "#ea4c88", "#993399", "#663399", "#333399", "#0066cc", "#0099cc", "#66cccc", "#77cc33", "#669900", "#336600", "#666600", "#999900", "#cccc33", "#ffff00", "#ffcc33", "#ff9900", "#ff6600", "#cc6633", "#996633", "#663300", "#000000", "#999999", "#cccccc", "#ffffff", "#E7D8B1", "#FDADC7", "#424153", "#ABBCDA", "#F5DD01"]';
