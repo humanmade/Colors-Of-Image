fc5e4b65afeec6d8f6e4aacc2863c5559e2087ff
Joe Hoyle <joehoyle@gmail.com>
2012-07-24 14:05:55 +0100

    use ciede2000 colour difference algorithm

--- colorsofimage.class.php
+++ colorsofimage.class.php
@@ -38,8 +38,7 @@ class ColorsOfImage {
 
 		$this->readPixels();
 
-		$bg_color = $this->getBackgroundColor();
-		$this->_excluded_colors[] = $this->RGBToHex( $bg_color[0], $bg_color[1], $bg_color[2] );;
+		$this->_excluded_colors[] = $this->getBackgroundColor();
 	}
 
 	public function setMinPercentage( $num ) {
@@ -96,7 +95,7 @@ class ColorsOfImage {
 		return true;
 	}
 
-	private function getBackgroundColor() {
+	public function getBackgroundColor() {
 
 		$top_left_color = imagecolorsforindex( $this->workingImage, imagecolorat( $this->workingImage, 0, 0) );
 		$top_left = $this->getClosestColor( $top_left_color['red'], $top_left_color['green'], $top_left_color['blue'] );
@@ -112,8 +111,9 @@ class ColorsOfImage {
 
 		$colors = array( $top_left, $top_right, $bottom_left, $bottom_right);
 
-		if( count( array_unique( $colors ) ) == 1 )
-			return $top_left;
+		if( count( array_unique( $colors ) ) == 1 ) {
+			return $this->RGBToHex( $top_left[0], $top_left[1], $top_left[2] );
+		}
 
 		return null;
 	}
@@ -203,8 +203,7 @@ class ColorsOfImage {
 	public function getProminentColors() {
 
 		$pixels 		= $this->getPercentageOfColors();
-		$bg_color 		= $this->getBackgroundColor();
-		$bg_color_hex 	= $this->RGBToHex( $bg_color[0], $bg_color[1], $bg_color[2] );
+		$bg_color_hex 	= $this->getBackgroundColor();
 
 
 		foreach ($pixels as $color => $value) {
@@ -219,6 +218,32 @@ class ColorsOfImage {
 		}
 
 		return $_c;
+	}
+
+	/**
+	 * Function used for testing, will return a map pof all colors to their matching color counter part
+	 */
+	public function getColorMap() {
+
+		$image 		= $this->image;
+		$width 		= $this->width;
+		$height 	= $this->height;
+		$arrayex 	= explode( '.', $image );
+		$typeOfImage= end( $arrayex );
+
+		for( $x = 0; $x < $width; $x += $this->precision ) {
+			for ( $y = 0; $y < $height; $y += $this->precision ) {
+				
+				$index = imagecolorat($this->workingImage, $x, $y);
+				$rgb = imagecolorsforindex($this->workingImage, $index);
+
+				$color = $this->getClosestColor( $rgb["red"], $rgb["green"], $rgb["blue"] );
+
+				$hexarray[ $this->RGBToHex( $rgb["red"], $rgb["green"], $rgb["blue"] ) ] = $this->RGBToHex( $color[0], $color[1], $color[2] );
+			}
+		}
+
+		return $hexarray;
 	}
 
 	private function getClosestColor($r, $g, $b){
@@ -252,7 +277,7 @@ class ColorsOfImage {
 		$lab1 = self::xyz_to_lab( $xyz1 );
 		$lab2 = self::xyz_to_lab( $xyz2 );
 
-		return self::de_1994( $lab2, $lab1 );
+		return ciede2000( $lab1, $lab2 );
 	} 
 
 	private function getPalette() {
@@ -299,7 +324,7 @@ class ColorsOfImage {
 		 $a= 500*($_x-$_y);
 		 $b= 200*($_y-$_z);
 		 
-		 return(array($l,$a,$b));
+		 return(array( 'l' => $l, 'a' => $a, 'b' => $b));
 	}
 
 
@@ -338,7 +363,7 @@ class ColorsOfImage {
 		$x = $_red * 0.4124 + $_green * 0.3576 + $_blue * 0.1805;
 		$y = $_red * 0.2126 + $_green * 0.7152 + $_blue * 0.0722;
 		$z = $_red * 0.0193 + $_green * 0.1192 + $_blue * 0.9505;
-		return(array($x,$y,$z));
+		return(array( $x, $y, $z ));
 	}
 
 
@@ -358,3 +383,122 @@ class ColorsOfImage {
 		return(sqrt($first*$first+$second*$second+$third*$third));
 	}
 }
+
+/**
+ * @author Markus Näsman
+ * @copyright 2012 (c) Markus Näsman <markus at botten dot org >
+ * @license see COPYING
+ */
+
+/**
+ * API FUNCTIONS
+ */
+
+/**
+* Returns diff between c1 and c2 using the CIEDE2000 algorithm
+* @return {float}   Difference between c1 and c2
+*/
+function ciede2000($c1,$c2) {
+  /**
+   * Implemented as in "The CIEDE2000 Color-Difference Formula:
+   * Implementation Notes, Supplementary Test Data, and Mathematical Observations"
+   * by Gaurav Sharma, Wencheng Wu and Edul N. Dalal.
+   */
+
+  // Get L,a,b values for color 1
+  $L1 = $c1['l'];
+  $a1 = $c1['a'];
+  $b1 = $c1['b'];
+
+  // Get L,a,b values for color 2
+  $L2 = $c2['l'];
+  $a2 = $c2['a'];
+  $b2 = $c2['b'];
+
+  // Weight factors
+  $kL = 1;
+  $kC = 1;
+  $kH = 1;
+
+  /**
+   * Step 1: Calculate C1p, C2p, h1p, h2p
+   */
+  $C1 = sqrt(pow($a1, 2) + pow($b1, 2)); //(2)
+  $C2 = sqrt(pow($a2, 2) + pow($b2, 2)); //(2)
+
+  $a_C1_C2 = ($C1+$C2)/2.0;             //(3)
+
+  $G = 0.5 * (1 - sqrt(pow($a_C1_C2 , 7.0) / (pow($a_C1_C2, 7.0) + pow(25.0, 7.0)))); //(4)
+
+  $a1p = (1.0 + $G) * $a1; //(5)
+  $a2p = (1.0 + $G) * $a2; //(5)
+
+  $C1p = sqrt(pow($a1p, 2) + pow($b1, 2)); //(6)
+  $C2p = sqrt(pow($a2p, 2) + pow($b2, 2)); //(6)
+
+  
+
+  $h1p = hp_f($b1, $a1p); //(7)
+  $h2p = hp_f($b2, $a2p); //(7)
+
+  /**
+   * Step 2: Calculate dLp, dCp, dHp
+   */
+  $dLp = $L2 - $L1; //(8)
+  $dCp = $C2p - $C1p; //(9)
+
+ 
+  $dhp = dhp_f($C1,$C2, $h1p, $h2p); //(10)
+  $dHp = 2*sqrt($C1p*$C2p)*sin(radians($dhp)/2.0); //(11)
+
+  /**
+   * Step 3: Calculate CIEDE2000 Color-Difference
+   */
+  $a_L = ($L1 + $L2) / 2.0; //(12)
+  $a_Cp = ($C1p + $C2p) / 2.0; //(13)
+
+   
+  
+  $a_hp = a_hp_f($C1,$C2,$h1p,$h2p); //(14)
+
+  $T = 1-0.17*cos(radians($a_hp-30))+0.24*cos(radians(2*$a_hp))+0.32*cos(radians(3*$a_hp+6))-0.20*cos(radians(4*$a_hp-63)); //(15)
+  $d_ro = 30 * exp(-(pow(($a_hp-275)/25,2))); //(16)
+  $RC = sqrt((pow($a_Cp, 7.0)) / (pow($a_Cp, 7.0) + pow(25.0, 7.0)));//(17)
+  $SL = 1 + ((0.015 * pow($a_L - 50, 2)) / sqrt(20 + pow($a_L - 50, 2.0)));//(18)
+  $SC = 1 + 0.045 * $a_Cp;//(19)
+  $SH = 1 + 0.015 * $a_Cp * $T;//(20)
+  $RT = -2 * $RC * sin(radians(2 * $d_ro));//(21)
+  $dE = sqrt(pow($dLp /($SL * $kL), 2) + pow($dCp /($SC * $kC), 2) + pow($dHp /($SH * $kH), 2) + $RT * ($dCp /($SC * $kC)) * ($dHp / ($SH * $kH))); //(22)
+  return $dE;
+}
+
+function hp_f($x,$y) //(7)
+  {
+	if($x== 0 && $y == 0) return 0;
+	else{
+	  $tmphp = degrees(atan2($x,$y));
+	  if($tmphp >= 0) return $tmphp;
+	  else           return $tmphp + 360;
+	}
+  }
+ function dhp_f($C1, $C2, $h1p, $h2p) //(10)
+  {
+	if($C1*$C2 == 0)               return 0;
+	else if(abs($h2p-$h1p) <= 180) return $h2p-$h1p;
+	else if(($h2p-$h1p) > 180)     return ($h2p-$h1p)-360;
+	else if(($h2p-$h1p) < -180)    return ($h2p-$h1p)+360;
+	else                         throw(error);
+  }
+  function a_hp_f($C1, $C2, $h1p, $h2p) { //(14)
+	if($C1*$C2 == 0)                                      return $h1p+$h2p;
+	else if(abs($h1p-$h2p)<= 180)                         return ($h1p+$h2p)/2.0;
+	else if((abs($h1p-$h2p) > 180) && (($h1p+$h2p) < 360))  return ($h1p+$h2p+360)/2.0;
+	else if((abs($h1p-$h2p) > 180) && (($h1p+$h2p) >= 360)) return ($h1p+$h2p-360)/2.0;
+	else                                                throw( new Exception( 'd' ));
+  }
+
+/**
+ * INTERNAL FUNCTIONS
+ */
+function degrees($n) { return $n*(180/pi()); }
+function radians($n) { return $n*(pi()/180); }
