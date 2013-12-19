446f6fa76b4fd5bd458613199e990aca45c1cc09
Brian Foxwell <brian@foxwell.io>
2013-12-19 02:42:08 -0500

    object integration test

--- tests/ImagePaletteTest.php
+++ tests/ImagePaletteTest.php
@@ -2,9 +2,9 @@
 
 class ImagePaletteTest extends PHPUnit_Framework_Testcase
 {
-    public function testImagePaletteIsArrayIntegration()
+    public function testIntegrationImagePaletteIsObject()
     {
         $image = new \bfoxwell\ImagePalette\ImagePalette('https://www.google.com/images/srpr/logo11w.png');
-        return $this->assertTrue($image);
+        return $this->assertTrue(is_object($image));
     }
 } 
