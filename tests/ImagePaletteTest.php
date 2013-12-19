3ed8ff5da68f3cfed3a20e79b92b0ee4a78fcf85
Brian Foxwell <brian@foxwell.io>
2013-12-19 02:48:06 -0500

    test integration of prominent colors, is array

--- tests/ImagePaletteTest.php
+++ tests/ImagePaletteTest.php
@@ -2,9 +2,23 @@
 
 class ImagePaletteTest extends PHPUnit_Framework_Testcase
 {
+    private $image;
+
+    public function setUp() {
+        $this->image = new \bfoxwell\ImagePalette\ImagePalette('https://www.google.com/images/srpr/logo11w.png');
+    }
+
+    public function tearDown() {
+        $this->image = null;
+    }
+
     public function testIntegrationImagePaletteIsObject()
     {
-        $image = new \bfoxwell\ImagePalette\ImagePalette('https://www.google.com/images/srpr/logo11w.png');
-        return $this->assertTrue(is_object($image));
+        return $this->assertTrue(is_object($this->image));
+    }
+
+    public function testIntegrationProminentColorsIsArray()
+    {
+        return $this->assertTrue(is_array($this->image->getProminentColors()));
     }
 } 
