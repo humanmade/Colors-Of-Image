c76f50f0bc5cdb123d80319aee01fc91764d3edc
Brian Foxwell <brian@foxwell.io>
2013-12-19 02:40:13 -0500

    updated test

--- tests/ImagePaletteTest.php
+++ tests/ImagePaletteTest.php
@@ -5,6 +5,6 @@ class ImagePaletteTest extends PHPUnit_Framework_Testcase
     public function testImagePaletteIsArrayIntegration()
     {
         $image = new \bfoxwell\ImagePalette\ImagePalette('https://www.google.com/images/srpr/logo11w.png');
-        return $this->assertTrue(is_array($image));
+        return $this->assertTrue($image);
     }
 } 
