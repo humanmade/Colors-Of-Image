acb8a56a86c92bf4caf5e0686d14101d0bcf76ca
Brian Foxwell <brian@foxwell.io>
2013-12-21 15:46:19 -0500

    updated headers

--- tests/ImagePaletteTest.php
+++ tests/ImagePaletteTest.php
@@ -5,6 +5,7 @@
  *
  * (c) Joe Hoyle <joe@hmn.md>
  * (c) Brian Foxwell <brian@foxwell.io>
+ * (c) Marc Pacheco <Marckk1997@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
